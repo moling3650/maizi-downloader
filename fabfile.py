8aa7c678c70da41815c53815db8f17458798b6d6
moling <365024424@qq.com>
2016-09-23 16:45:09 +0800

    隐藏open和BeautifulSoup的任务，支持带参数任务
    凡是from xxx import ooo的，fabric都认为是它的子任务组，所以把io和bs4改成import的形式了。
    支持带参数任务！昨晚想一夜也没搞明白，原来是fab task:arg的方式运行

--- fabfile.py
+++ fabfile.py
@@ -6,8 +6,8 @@
 # @Version : $Id$
 import os
 import requests
-from io import open
-from bs4 import BeautifulSoup
+import io
+import bs4
 from fabric.api import *
 
 BASE_DIR = os.getcwd()
@@ -15,15 +15,18 @@ TEMP_DIR = os.path.join(BASE_DIR, 'temp')
 URL = 'http://www.maiziedu.com/course/%d/'
 
 
-def download():
-    num = int(raw_input())
+@task(alias='dl')
+def download(num):
+    '''
+    下载课程，要求一个参数num：课程编号
+    '''
     # 抓取课程主页的内容写content.txt文件
-    r = requests.get(URL % num)
-    with open('content.txt', 'wb') as f:
+    r = requests.get(URL % int(num))
+    with io.open('content.txt', 'wb') as f:
         f.write(r.content)
 
     # 用BeautifulSoup提取课程的所有连接
-    soup = BeautifulSoup(open('content.txt', encoding='utf-8'), 'html.parser')
+    soup = bs4.BeautifulSoup(io.open('content.txt', encoding='utf-8'), 'html.parser')
     lesson_lists = soup.select('.lesson-lists > li > a')
 
     data = {}
@@ -36,12 +39,11 @@ def download():
 
     for name, url in data.items():
         with lcd(BASE_DIR):
-            local('you-get %s' % url)
-            file = filter(lambda f: f.endswith(('mp4', 'None')), os.listdir(BASE_DIR)).pop()
-            local('move %s %s' % (file, TEMP_DIR))
+            local('you-get -O temp.mp4 %s' % url)
+            local('move temp.mp4 %s' % (TEMP_DIR))
 
         with lcd(TEMP_DIR):
-            local('move %s %s.mp4' % (file, ''.join(name.split())))
+            local('move temp.mp4 %s.mp4' % (''.join(name.split())))
 
     with lcd(BASE_DIR):
         local('ren temp ' + soup.title.text.encode('gbk'))
