55469eba4e7af4d4195e70425bd362c0f629c66e
moling <365024424@qq.com>
2016-09-23 08:10:29 +0800

    封装成一个任务，可以通过输入课程的编号进行下载，而不能输入完整的URL

--- fabfile.py
+++ fabfile.py
@@ -12,31 +12,32 @@ from fabric.api import *
 
 BASE_DIR = os.getcwd()
 TEMP_DIR = os.path.join(BASE_DIR, 'temp')
-URL = 'http://www.maiziedu.com/course/641/'
-
-# 抓取课程主页的内容写content.txt文件
-r = requests.get(URL)
-with open('content.txt', 'wb') as f:
-    f.write(r.content)
-
-# 用BeautifulSoup提取课程的所有连接
-soup = BeautifulSoup(open('content.txt', encoding='utf-8'), 'html.parser')
-lesson_lists = soup.select('.lesson-lists > li > a')
-
-data = {}
-for lesson in lesson_lists:
-    url = 'http://www.maiziedu.com' + lesson.get('href')
-    data[lesson.find(class_='fl').text.encode('gbk')] = url
+URL = 'http://www.maiziedu.com/course/%d/'
 
 
 def download():
+    num = int(raw_input())
+    # 抓取课程主页的内容写content.txt文件
+    r = requests.get(URL % num)
+    with open('content.txt', 'wb') as f:
+        f.write(r.content)
+
+    # 用BeautifulSoup提取课程的所有连接
+    soup = BeautifulSoup(open('content.txt', encoding='utf-8'), 'html.parser')
+    lesson_lists = soup.select('.lesson-lists > li > a')
+
+    data = {}
+    for lesson in lesson_lists:
+        url = 'http://www.maiziedu.com' + lesson.get('href')
+        data[lesson.find(class_='fl').text.encode('gbk')] = url
+
     if not os.path.exists(TEMP_DIR):
         local('mkdir ' + TEMP_DIR)
 
     for name, url in data.items():
         with lcd(BASE_DIR):
             local('you-get %s' % url)
-            file = filter(lambda f: f.endswith('mp4'), os.listdir(BASE_DIR)).pop()
+            file = filter(lambda f: f.endswith(('mp4', 'None')), os.listdir(BASE_DIR)).pop()
             local('move %s %s' % (file, TEMP_DIR))
 
         with lcd(TEMP_DIR):
